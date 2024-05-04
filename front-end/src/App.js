e15df4716ffd866cbb91e957ecee0c155989c128
Elusionz243 <strivelinkband@yahoo.com>
2024-05-03 23:27:18 -0500

    Continuing Solana integration

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,31 @@
 import { init, useConnectWallet } from "@web3-onboard/react";
 import phantomModule from "@web3-onboard/phantom";
-import { Keypair } from "@solana/web3.js";
-import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import {
+  Keypair,
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
 
 import "./App.scss";
 
 const phantom = phantomModule();
-const ethApiKey = process.env.ETHERIUM_API_KEY;
-const solApiKey = process.env.SOLANA_API_KEY;
+const ethApiKey = process.env.REACT_APP_PHANTOM_API_KEY;
+const solApiKey = process.env.REACT_APP_SOLANA_API_KEY;
 
-const keypair = getKeypairFromEnvironment(process.env.SECRET_KEY);
+const connection = new Connection(clusterApiUrl("devnet"));
+const address = new PublicKey("BkuexoV2sfmCgKgNyiEux6Xj8Yob2sYfsrHFDLk5LyMt");
+const balance = await connection.getBalance(address);
+const balanceInSol = balance / LAMPORTS_PER_SOL;
+
+const keypair = Keypair.generate();
+console.log(keypair.publicKey.toBase58());
+console.log(keypair.secretKey.toString());
+
+console.log(
+  `Connecting to Ethereum Mainnet with API key: ${ethApiKey}\nConnecting to Solana Mainnet with API key: ${solApiKey}`
+);
 
 init({
   wallets: [phantom],
@@ -37,31 +53,22 @@ init({
 
 export default function App() {
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
-  console.log(connecting);
-  // if (connecting) {
-  //   console.log(wallet.accounts[0].balance);
-  // }
 
   return (
     <div className="App">
       <div className="App__header">
         <h1>Cryptonic</h1>
+      </div>
+      <div className="App__content">
         <button
           disabled={connecting}
           onClick={() => (wallet ? disconnect(wallet) : connect())}
         >
           {connecting ? "connecting" : wallet ? "disconnect" : "connect"}
         </button>
-      </div>
-      <div className="App__content">
-        {/* {balance !== null && (
-          <div className="App__content-item">
-            <h2>Account Balance</h2>
-            <p>{balance} ETH</p>
-          </div>
-        )} */}
-        {/* {blockNumber} */}
-        {/* {chainId} */}
+        <h4>Address: {address.toBase58()}</h4>
+        <p>Balance: {balance}</p>
+        <p>Balance in SOL: {balanceInSol} SOL</p>
       </div>
       <div className="App__footer">
         <p>&copy; 2024 Cryptonic. All rights reserved.</p>
