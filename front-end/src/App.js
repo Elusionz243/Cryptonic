f69c8c332af1a802f61b5bf1d5f9fc8514039baa
Elusionz243 <strivelinkband@yahoo.com>
2024-05-04 23:48:58 -0500

    Added Wallet Providers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,14 @@
-import { init, useConnectWallet } from "@web3-onboard/react";
-import phantomModule from "@web3-onboard/phantom";
+// import { init, useConnectWallet } from "@web3-onboard/react";
+// import phantomModule from "@web3-onboard/phantom";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import * as web3 from "@solana/web3.js";
 import {
   Keypair,
   Connection,
@@ -9,65 +18,43 @@ import {
 } from "@solana/web3.js";
 
 import "./App.scss";
+import { useMemo } from "react";
 
-const phantom = phantomModule();
-const ethApiKey = process.env.REACT_APP_PHANTOM_API_KEY;
-const solApiKey = process.env.REACT_APP_SOLANA_API_KEY;
+const { REACT_APP_SECRET_KEY } = process.env;
 
 const connection = new Connection(clusterApiUrl("devnet"));
 const keypair = Keypair.fromSecretKey(
-  new Uint8Array(JSON.parse(process.env.REACT_APP_SECRET_KEY))
+  new Uint8Array(JSON.parse(REACT_APP_SECRET_KEY))
 );
 const address = new PublicKey(keypair.publicKey.toBase58());
-console.log(address.toBase58());
 const balance = await connection.getBalance(address);
 const balanceInSol = balance / LAMPORTS_PER_SOL;
 
-init({
-  wallets: [phantom],
-  chains: [
-    {
-      id: "0x1",
-      token: "ETH",
-      label: "Ethereum Mainnet",
-      rpcUrl: `https://eth-mainnet.g.alchemy.com/v2/${ethApiKey}`,
-    },
-    {
-      id: "900",
-      token: "SOL",
-      label: "Solana Mainnet",
-      rpcUrl: `https://solana-mainnet.g.alchemy.com/v2/${solApiKey}`,
-    },
-    {
-      id: "0x2105",
-      token: "ETH",
-      label: "Base",
-      rpcUrl: "https://mainnet.base.org",
-    },
-  ],
-});
-
 export default function App() {
-  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
-
+  // NEW IMPLEMENTATION (PHANTOM)
+  const endpoint = web3.clusterApiUrl("devnet");
+  const wallets = useMemo(() => [], []);
   return (
-    <div className="App">
-      <div className="App__header">
-        <h1>Cryptonic</h1>
-      </div>
-      <div className="App__content">
-        <button
-          disabled={connecting}
-          onClick={() => (wallet ? disconnect(wallet) : connect())}
-        >
-          {connecting ? "connecting" : wallet ? "disconnect" : "connect"}
-        </button>
-        <h4>Address: {address.toBase58()}</h4>
-        <p>{balanceInSol.toPrecision(10)} SOL</p>
-      </div>
-      <div className="App__footer">
-        <p>&copy; 2024 Cryptonic. All rights reserved.</p>
-      </div>
-    </div>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <div className="App">
+          <div className="App__header">
+            <h1>Cryptonic</h1>
+          </div>
+          <div className="App__content">
+            <h3>Address: {address.toBase58()}</h3>
+            <p>{balanceInSol.toPrecision(10)} SOL</p>
+            <WalletModalProvider>
+              <WalletMultiButton>
+                <div className="App__wallet-btn">Connect Wallet</div>
+              </WalletMultiButton>
+            </WalletModalProvider>
+          </div>
+          <div className="App__footer">
+            <p>&copy; 2024 Cryptonic. All rights reserved.</p>
+          </div>
+        </div>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
